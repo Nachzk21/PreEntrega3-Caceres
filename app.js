0279537c118eb633a98ce4396ec314a296ca83f0
Ignacio Caceres <ignacionahuel1245@gmail.com>
2023-05-29 23:18:30 -0300

    tercer commit

--- app.js
+++ app.js
@@ -1,60 +1,141 @@
-
 alert("Bienvenido a mi pagina, por el momento tu usuario y contraseña para ingresar seran admin y 45623 ")
 
 
-// Inicio de sesion
+//Inicio de sesion
 
-let usuario = prompt("Bienvenido a mi primer pre entrega, para continuar por favor ingrese el nombre de usuario proporcionado")
-let contraseña = Number(prompt("Ahora ingrese la contraseña"))
+function validarUsuario() 
+{
+const user = "admin"
+const password = "45623"
+let userOK = false
 
-if (usuario === "admin" && contraseña == "45623") {
-    alert("Ingreso con exito")
-} else {
-    alert("Usuario o contraseña no valido")
+while (userOK === false){
+    let usuarioIngresado = prompt("Ingrese el usuario proporcinado")
+    let passwordIngresada = prompt("Ingrese la contraseña proporcionada")
+
+    if (usuarioIngresado === user && passwordIngresada === password) {
+        alert("Bienvenido" + " " + usuarioIngresado)
+        userOK = true
+    } else {
+        alert("Usuario o contraseña incorrectos")
+        reintentar = confirm("Desea volver a intentarlo")
+        if(reintentar){
+            userOK = false
+            break
+        }
+    }
+}
+}
+
+validarUsuario()
+
+// Constructor de objetos
+
+class autoNuevo{
+    constructor(marca, modelo, color){
+    this.marca = marca
+    this.modelo = modelo
+    this.color = color
+    }
 }
 
 
-// Catalogo
 
-let catalogoPrimero = prompt("Desea acceder al catalogo?")
 
-if (catalogoPrimero == "si" && "Si"){
-do {
-    let autos = prompt
-        (` Por favor, elija una marca de autos de la cual desee saber el precio de sus modelos mas recientes o escriba salir para cerrar esta ventana
-    -Ford
-    -Chevrolet
-    -Fiat`)
+// Objeto de arryas
+let catalogo = [
+    {marca: "Ford", modelo: "Fiesta", año: "2008", color: "blanco", precio: "U$S 8500"},
+    {marca: "Ford", modelo: "EcoSport", año: "2013", color: "negro", precio: "U$S 13.500"},
+    {marca: "Chevrolet", modelo: "Camaro", año: "2014", color: "amarillo", precio: "U$S 59.500"},
+    {marca: "Chevrolet", modelo: "Corsa", año: "2009", color: "gris", precio: "U$S 3800"},
+    {marca: "Fiat", modelo: "Cronos", año: "2023", color: "blanco", precio: "U$S 32.520"},
+    {marca: "Fiat", modelo: "147", año: "1983", color: "rojo", precio: "U$S 21.000"},
+]
 
-    switch (autos) {
-        case "Ford" && "ford":
-            alert(` Los precios para la marca Ford son los siguientes:
-            Ford F-150: U$S 113.000
-            Ford Msutang: U$S 74.000
-            Ford Fiesta: U$S 7.985
-            `)
-            break;
-        case "Chevrolet" && "chevrolet":
-            alert(`Los precios para la marca Chevrolet son los siguientes:
-            Chevrolet Onix: U$S 7.441
-            Chevrolet Cruze: U$S 23.454
-            Chevrolet Joy: U$S 10.874,20         `)
-            break;
-        case "Fiat" && "fiat":
-            alert(`Los precios para la marca Chevrolet son los siguientes:
-            Fiat Toro: U$S 27.697
-            Fiat Strada: U$S 6.396
-            Fiat 500 Abarth: U$S 23.000`)
-            break;
-        default:
-            alert("Marca Invalida")
-            break;
+// Recorrer el array
+function mostrarFord(){
+    const ford = catalogo.filter((e)=> e.marca.includes ("Ford") )
+    modelosDisponibles = 'Los Modelos disponibles para la marca Ford son: \n';
+    for (const vehiculo of ford) {
+        modelosDisponibles += `${vehiculo.marca} ${vehiculo.modelo} ${vehiculo.año}. Color ${vehiculo.color} con un precio de ${vehiculo.precio} \n`
     }
 
-    catalogoPrimero = prompt("Desea seguir viendo el catalogo?")
-} while (catalogoPrimero != "no");
-}else{
-    alert ("Gracias por su visita")
+    alert(modelosDisponibles)
 }
+
+function mostrarChevrolet(){
+    const chevrolet = catalogo.filter((e)=> e.marca.includes ("Chevrolet") )
+    modelosDisponibles = 'Los Modelos disponibles para la marca Chevrolet son: \n';
+    for (const vehiculo of chevrolet) {
+        modelosDisponibles += `${vehiculo.marca} ${vehiculo.modelo} ${vehiculo.año}. Color ${vehiculo.color} con un precio de ${vehiculo.precio} \n`
+    }
+
+    alert(modelosDisponibles)
+}
+
+function mostrarFiat(){
+    const fiat = catalogo.filter((e)=> e.marca.includes ("Fiat") )
+    modelosDisponibles = 'Los Modelos disponibles para la marca Fiat son: \n';
+    for (const vehiculo of fiat) {
+        modelosDisponibles += `${vehiculo.marca} ${vehiculo.modelo} ${vehiculo.año}. Color ${vehiculo.color} con un precio de ${vehiculo.precio} \n`
+    }
+
+    alert(modelosDisponibles)
+}
+
+
+// Funcion para construir un objeto
+
+function sugerirProducto() {
+    let marca = prompt("De que marca deberia ser el nuevo vehiculo?")
+    let modelo = prompt(`Y que modelo de la marca ${marca} deberiamos traer?` )
+    let color = prompt("Algun color en especifico?") 
+
+    const nuevoAuto = new autoNuevo(marca,modelo,color)
+
+    arrayNuevoAuto.push(nuevoAuto)
+    alert("Gracias por su sugerencia!")
+}
+
+//Funcion para mostrar productos recorriendo el array
+
+
+function sugerencias(){
+    arrayNuevoAuto.forEach((e)=> {
+        alert (`Hola! anteriormente sugeriste los siguientes autos para añadir a nuestro catalogo: \n Marca: ${e.marca} \n Modelo: ${e.modelo} \n Color: ${e.color}` )
+    })
+}
+
+let arrayNuevoAuto = []
+
+// Opciones para el usuario
+
+let opciones = prompt("Ingrese una opcion para continuar: \n 1: Ver catalogo Ford \n 2: Ver catalogo Fiat \n 3: Ver catalogo Chevrolet \n 4: Sugerir un nuevo para añadir al catalogo \n 5: Ver sugerencias \n 6: Finalizar ")
+
+while(opciones !== "6") 
+{
+    if (opciones === "1"){
+        mostrarFord()
+    }
+    if (opciones === "2"){
+        mostrarFiat()
+    }
+    if (opciones === "3"){
+        mostrarChevrolet()
+    }
+    if (opciones === "4"){
+        sugerirProducto()
+    }
+    if (opciones === "5" ){
+        sugerencias()
+    }
+    opciones = prompt("Vuelva a ingresar una opcion \n 1: Ver catalogo Ford \n 2: Ver catalogo Fiat \n 3: Ver catalogo Chevrolet \n 4: Sugerir un nuevo para añadir al catalogo \n 5: Ver sugerencias \n 6: Finalizar  ")
+}
+
+
+
+
+
+
 
 
